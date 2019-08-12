cbec1127621b90e5e7035442d9a77878225a58fa
kido-k <kotaro.k13@gmail.com>
2019-08-12 12:39:07 +0900

    formのフォーマットファイルを作成

---
+++ app/assets/const/formFormat.js
@@ -0,0 +1,40 @@
+export default {
+  main: {
+    title: '',
+    detail: ''
+  },
+  name: {
+    displayName: true,
+    familyNameLabel: '姓',
+    familyNamePlaceHolder: '田中',
+    firstNameLabel: '名',
+    firstNamePlaceHolder: '太郎'
+  },
+  nameKana: {
+    displayNameKana: true,
+    familyNameLabel: 'セイ',
+    familyNamePlaceHolder: 'タナカ',
+    firstNameLabel: 'メイ',
+    firstNamePlaceHolder: 'タロウ'
+  },
+  address: {
+    displayAddress: true,
+    zipCode: '',
+    prefecture: '',
+    city: '',
+    building: ''
+  },
+  telephone: {
+    displayTelephone: true,
+    telephone: ''
+  },
+  mail: {
+    displayMail: true,
+    mailAddress: ''
+  },
+  finish: {
+    finishMessage: '',
+    finishButton: '',
+    finishUrl: ''
+  }
+}
